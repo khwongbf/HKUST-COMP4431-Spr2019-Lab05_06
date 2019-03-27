cb81f89544bee86446055f3d50455d8bec08726d
khwongbf <khwongbf@github.com>
2019-03-27 15:25:50 +0800

    finished dither.js

--- js/dither.js
+++ js/dither.js
@@ -49,7 +49,7 @@
 				matrix = [ [1, 9, 3, 11], [13, 5, 15, 7], [4, 12, 2, 10], [16, 8, 14, 6] ];
 				levels = 17;
 
-				// The following code uses Bayer's 2x2 matrix to create the
+				// The following code uses Bayer's 4x4 matrix to create the
 				// dithering effect. You need to extend it to work for different
 				// matrix types
 
@@ -73,9 +73,61 @@
 				}
 				break;
 			case "line":
-				...
+				// Set up the matrix
+				matrix = [ [15, 15, 15, 25], [15, 15, 25, 15], [15, 25, 15, 15], [25, 15, 15, 15] ];
+				levels = 100;
+
+				// The following code uses line matrix to create the
+				// dithering effect. You need to extend it to work for different
+				// matrix types
+
+				for (var y = 0; y < inputData.height; y++) {
+					for (var x = 0; x < inputData.width; x++) {
+						var pixel = imageproc.getPixel(inputData, x, y);
+
+						/* Change the colour to grayscale and normalize it */
+						var value = (pixel.r + pixel.g + pixel.b) / 3;
+						value = value / 255 * levels;
+
+						/* Get the corresponding threshold of the pixel */
+						var threshold = matrix[y % 4][x % 4];
+
+						/* Set the colour to black or white based on threshold */
+						var i = (x + y * outputData.width) * 4;
+						outputData.data[i]     =
+						outputData.data[i + 1] =
+						outputData.data[i + 2] = (value < threshold)? 0 : 255;
+					}
+				}
+				break;
 			case "diamond":
-				...
+				// Set up the matrix
+				matrix = [ [15, 25, 15, 25], [25, 15, 25, 15], [15, 25, 15, 25], [25, 15, 25, 15] ];
+				levels = 100;
+
+				// The following code uses diamond matrix to create the
+				// dithering effect. You need to extend it to work for different
+				// matrix types
+
+				for (var y = 0; y < inputData.height; y++) {
+					for (var x = 0; x < inputData.width; x++) {
+						var pixel = imageproc.getPixel(inputData, x, y);
+
+						/* Change the colour to grayscale and normalize it */
+						var value = (pixel.r + pixel.g + pixel.b) / 3;
+						value = value / 255 * levels;
+
+						/* Get the corresponding threshold of the pixel */
+						var threshold = matrix[y % 4][x % 4];
+
+						/* Set the colour to black or white based on threshold */
+						var i = (x + y * outputData.width) * 4;
+						outputData.data[i]     =
+						outputData.data[i + 1] =
+						outputData.data[i + 2] = (value < threshold)? 0 : 255;
+					}
+				}
+				break;
 		}
     }
  
